95b835d03f70cafab9dbdaff641d2d700d9c5396
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-20 19:37:45 -0700

    Add test for Deck for function, card property to instantiate a new instance of card, and a method tocount the cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,32 +1,41 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const Deck = require("../src/Deck");
 const Card = require("../src/Card");
+const Deck = require("../src/Deck");
 
 describe("Deck", () => {
-    let card1, card2, card3, deck
-    
+    let card1, card2, card3, deck;
+
     beforeEach(() => {
         card1 = new Card(
-          1,
-          "What is Robbie's favorite animal",
-          ["sea otter", "pug", "capybara"],
-          "sea otter"
+            1,
+            "What is Robbie's favorite animal",
+            ["sea otter", "pug", "capybara"],
+            "sea otter"
         );
         card2 = new Card(
-          14,
-          "What organ is Khalid missing?",
-          ["spleen", "appendix", "gallbladder"],
-          "gallbladder"
+            14,
+            "What organ is Khalid missing?",
+            ["spleen", "appendix", "gallbladder"],
+            "gallbladder"
         );
         card3 = new Card(
-          12,
-          "What is Travis's middle name?",
-          ["Lex", "William", "Fitzgerald"],
-          "Fitzgerald"
+            12,
+            "What is Travis's middle name?",
+            ["Lex", "William", "Fitzgerald"],
+            "Fitzgerald"
         );
 
         deck = new Deck([card1, card2, card3])
+    })
+    it("should be a function", () => {
+        expect(Deck).to.be.a("function")
+    })
+    it("should be an instance of deck", () => {
+        expect(deck).to.be.an.instanceOf(Deck)
+    })
+    it("should have a method to count how many cards are in the deck", () => {
+        expect(deck.countCards()).to.equal(3)
     })
 })
