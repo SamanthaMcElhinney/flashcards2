66676e7761126ba4b825fd598052c970ee915eff
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-20 21:24:31 -0700

    Add method to return current current

--- src/Round.js
+++ src/Round.js
@@ -1,14 +1,25 @@
 const Deck = require("./Deck")
 const Turn = require("./Turn")
 
-class Round{
-    constructor(deck){
+class Round {
+    constructor(deck) {
         this.deck = deck.cards
         this.currentCard = this.deck[0]
+        this.turns = 0
+        this.incorrectGuesses = []
     }
 
-    returnCurrentCard(){
-         return this.currentCard
+    returnCurrentCard() {
+        return this.currentCard
+    }
+
+    takeTurn(guess){
+        let turn = new Turn(guess, this.currentCard)
+        turn.evaluateGuess()
+        this.turns ++
+        if(!turn.evaluateGuess()) {
+            this.incorrectGuesses.push(this.currentCard)
+        }
     }
 }
 
