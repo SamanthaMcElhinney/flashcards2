70e98cf02a81667ccabe896cb02d0e5d27f5eae8
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-21 19:52:03 -0700

    Refactor functionality for updating deck

--- src/Round.js
+++ src/Round.js
@@ -6,6 +6,7 @@ class Round {
         this.deck = deck.cards
         this.currentCard = this.deck[0]
         this.turns = 0
+        this.time = Date.now()
         this.incorrectGuesses = []
     }
 
@@ -15,19 +16,28 @@ class Round {
 
     takeTurn(guess) {
         let turn = new Turn(guess, this.currentCard)
-        turn.evaluateGuess()
-        this.turns++
         if (!turn.evaluateGuess()) {
             this.incorrectGuesses.push(this.currentCard.id)
         }
+        this.turns++
+        if(this.turns < this.deck.length) {
+            this.currentCard = this.deck[this.turns]
+        }
         return turn.giveFeedback()
     }
-    calculatePercentCorrect(){
-        //incorrectGuesses.length, this.turns
+
+    calculatePercentCorrect() {
         let wins = this.turns - this.incorrectGuesses.length
-        let percent = (wins/this.turns) * 100
-        return percent.toFixed(2)
+        let percent = parseInt(((wins / this.turns) * 100).toFixed(2));
+        return percent;
     }
+
+    endRound() {
+        // console.timeLog("You played the game in")
+        return( `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
+    }
+
+
 }
 
-    module.exports = Round
+module.exports = Round
