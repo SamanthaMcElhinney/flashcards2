6f3246a63c0ba505408d5360a42f1268519679ac
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-21 19:51:41 -0700

    Add properties for game for current round and deck, update methods for createCards, deck, round and to start the game

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,47 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require("./Deck");
+const Round = require("./Round")
+const Card = require("./Card")
 
 class Game {
-  constructor() {}
-
+  constructor() {
+    this.currentRound;
+    this.deck;
+  }
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
+    -----------------------------------------------------------------------`)
+  }
+  
+  printQuestion(round) {
+    util.main(round);
   }
 
-  printQuestion(round) {
-      util.main(round);
+  createCards(){
+    let cards = prototypeQuestions.map((card) => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer);
+    });
+    return cards
   }
+
+  createDeck(){
+   this.deck = new Deck(this.createCards())
+   return this.deck
+  }
+
+  createRound(){
+    this.currentRound = new Round(this.createDeck())
+    return this.currentRound
+  }
+
+  start(){
+    this.printMessage(this.createDeck(), this.createRound)
+    this.printQuestion(this.createRound())
+  }
+  
+
 }
 
 module.exports = Game;
