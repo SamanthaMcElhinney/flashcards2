a96dd2eaf25a74c275321daf206d116ba6070596
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-03-22 20:23:32 -0700

    import game and start game feature to index class

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // This is where your project starts.
+const Game = require("./src/Game")
+const game = new Game()
 
+game.start()
+
+
+console.time("You played the game in")
 console.log('Your project is running...'); 
